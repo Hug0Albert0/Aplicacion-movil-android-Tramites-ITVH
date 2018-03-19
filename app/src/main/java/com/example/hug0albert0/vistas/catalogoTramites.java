fdf6c400cbb1a7e129f29656520f0ebeee6be03e
HugoAlber0 <hugorivera.cool.player@gmail.com>
2018-03-18 23:51:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/hug0albert0/vistas/catalogoTramites.java
@@ -0,0 +1,13 @@
+package com.example.hug0albert0.vistas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class catalogoTramites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalogo_tramites);
+    }
+}
---
+++ app/src/main/java/com/example/hug0albert0/vistas/validacion.java
@@ -0,0 +1,13 @@
+package com.example.hug0albert0.vistas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class validacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validacion);
+    }
+}
