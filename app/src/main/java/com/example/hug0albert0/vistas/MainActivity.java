0100862aa705d8d8a713fdbac7aa644622efcfdb
HugoAlber0 <hugorivera.cool.player@gmail.com>
2018-03-19 21:53:14 -0600

    Realización de Activity de Login

--- app/src/main/java/com/example/hug0albert0/vistas/MainActivity.java
+++ app/src/main/java/com/example/hug0albert0/vistas/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.hug0albert0.vistas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    private Button btnLogin;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin=findViewById(R.id.btnlogin);
+    }
+
+
+    public void lanzarlogin(View view)
+    {
+        Intent iLogin= new Intent(this, login.class);
+        startActivity(iLogin);
     }
 }
